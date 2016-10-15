9faba5ad62d8b93330c2774a0c1ab13698c5c80f
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-15 14:44:22 +0530

    Adds map:create command

---
+++ cmds/mapcreate.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(program) {
+
+	program
+		.command('map:create')
+		.version('0.0.0')
+        .description('Generates postgres to contentful mapping file')
+		.action(function (/* Args here */) {
+			// Your code goes here
+		});
+
+};
