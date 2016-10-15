c2bc1cdb91706b836c8146cbd9254fa7f9d29ef7
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-16 04:50:44 +0530

    Check if the output file already exists, if yes prompts to overwrite

--- cmds/map:create/index.js
+++ cmds/map:create/index.js
@@ -2,9 +2,23 @@
 
 const co = require('co');
 const chalk = require('chalk');
+const fs = require('fs');
+const inquirer = require('inquirer');
 const jsonfile = require('jsonfile');
 const getTableSchema = require('./get_database_table_schema');
 const getContentTypeSchema = require('./get_contentful_content_type_schema');
+
+function overwriteFile(filename) {
+    const question = {
+        name: 'overwrite',
+        type: 'confirm',
+        message: `Overwrite ${filename}:`,
+        default: false,
+        when: fs.statSync(filename).isFile(),
+    };
+
+    return inquirer.prompt(question);
+}
 
 module.exports = (program) => {
     const logger = program.log;
@@ -28,6 +42,12 @@ module.exports = (program) => {
         try {
             const contentfulAccessToken = program.config.get('access_token');
             const databaseConnectionURI = program.config.get('pg_connection_uri');
+
+            const {overwrite} = yield overwriteFile(output);
+
+            if (overwrite === false) {
+                exit(new Error('Output file already exists'));
+            }
 
             const tableSchema = yield getTableSchema(databaseConnectionURI);
             const contentTypeSchema = yield getContentTypeSchema(contentfulAccessToken);
