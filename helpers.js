4ce99d1e50ffa66e522542ab33f5bfa2700b9b3e
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-15 20:29:57 +0530

    Adds async helper

---
+++ helpers.js
@@ -0,0 +1,19 @@
+module.exports = {
+    async(makeGenerator){
+        return () => {
+            var generator = makeGenerator.apply(this, arguments);
+
+            function handle(result) { // { done: [Boolean], value: [Object] }
+                if (result.done) return result.value;
+
+                return result.value.then(function (res) {
+                    return handle(generator.next(res))
+                }, function (err) {
+                    return handle(generator.throw(err))
+                })
+            }
+
+            return handle(generator.next())
+        }
+    }
+};
