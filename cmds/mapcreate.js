8ae7d8bdae0dfae8606fb6b13ddf41afe381f4c7
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-15 16:43:17 +0530

    Adds inquirer, PostgresSchema. Adds code to extract schema of a valid postgres connection

--- cmds/mapcreate.js
+++ cmds/mapcreate.js
@@ -1,13 +1,52 @@
 'use strict';
 
-module.exports = function(program) {
+const inquirer = require('inquirer');
+const PostgresSchema = require('pg-json-schema-export');
 
-	program
-		.command('map:create')
-		.version('0.0.0')
+function getDatabaseSchema({postgres}) {
+    return PostgresSchema.toJSON(postgres, 'public');
+}
+
+function getDatabaseInformation() {
+    const question = {
+        name: 'postgres',
+        type: 'input',
+        message: 'Enter your postgreSQL connection string:',
+        validate(databaseConnectionString) {
+            if (databaseConnectionString.length) {
+                return true;
+            }
+            return 'Please enter your postgreSQL connection string';
+        }
+    };
+
+    return inquirer.prompt(question)
+}
+
+module.exports = (program) => {
+    const logger = program.log;
+
+    function run() {
+        getDatabaseInformation()
+            .then(getDatabaseSchema)
+            .then(console.log)
+            .then(exit)
+            .catch(exit);
+    }
+
+    function exit(err) {
+        if (!err) {
+            process.exit(0);
+        }
+
+        logger.error(`${err.code}: ${err.message}`);
+        process.exit(1);
+    }
+
+    program
+        .command('map:create')
+        .version('0.0.0')
         .description('Generates postgres to contentful mapping file')
-		.action(function (/* Args here */) {
-			// Your code goes here
-		});
+        .action(run);
 
 };
