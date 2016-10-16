e0715c9e426901fa8530fac246ddc50a9fba56bd
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-16 07:04:36 +0530

    Checks the mapping file for structure

---
+++ cmds/data:sync/check_mapping_data.js
@@ -0,0 +1,81 @@
+const _ = require('lodash');
+
+function checkKeys(mapping) {
+    const keysToCheck = [
+        'tableSchema',
+        'tableSchema.columns',
+        'contentTypeSchema',
+        'contentTypeSchema.fields',
+        'mappings',
+    ];
+
+    const isMissing = (key) => !_.has(mapping, key);
+    const missingElement = _.find(keysToCheck, isMissing);
+
+    if (missingElement) {
+        throw new Error(`${missingElement} missing`);
+    }
+
+    const isEmpty = (key) => _.isEmpty(_.result(mapping, key));
+    const emptyElement = _.find(keysToCheck, isEmpty);
+
+    if (emptyElement) {
+        throw new Error(`${emptyElement} is empty`);
+    }
+}
+
+function checkKeyTypes(mapping) {
+    let key = 'tableSchema.columns';
+    if (!_.isPlainObject(_.result(mapping, key))) {
+        throw new Error(`${key} should be plain object`);
+    }
+
+    key = 'contentTypeSchema.fields';
+    if (!_.isArray(_.result(mapping, key))) {
+        throw new Error(`${key} should be an array`);
+    }
+
+    key = 'mappings';
+    if (!_.isPlainObject(_.result(mapping, key))) {
+        throw new Error(`${key} should be plain object`);
+    }
+}
+
+function checkMappingValues(mapping) {
+    let isInvalidColumn = (value) => !_.isString(value);
+    let invalidColumn = _.findKey(mapping.mappings, isInvalidColumn);
+
+    if (invalidColumn) {
+        const value = mapping.mappings[invalidColumn];
+        throw new Error(`${invalidColumn} has a non string value (${value}) in mappings`);
+    }
+
+    isInvalidColumn = (value) => !_.has(mapping.tableSchema.columns, value);
+    invalidColumn = _.findKey(mapping.mappings, isInvalidColumn);
+
+    if (invalidColumn) {
+        const value = mapping.mappings[invalidColumn];
+        throw new Error(`${invalidColumn} has a value (${value}) with non existent column in db`);
+    }
+}
+
+function checkMappingKeys(mapping) {
+    const contentFields = _.map(mapping.contentTypeSchema.fields, 'id');
+    const invalidContentField = _
+        .chain(mapping.mappings)
+        .keys()
+        .difference(contentFields)
+        .first()
+        .value();
+
+    if (invalidContentField) {
+        throw new Error(`${invalidContentField} content field does not exists`);
+    }
+}
+
+module.exports = (mapping) => {
+    checkKeys(mapping);
+    checkKeyTypes(mapping);
+    checkMappingValues(mapping);
+    checkMappingKeys(mapping);
+};
--- cmds/data:sync/index.js
+++ cmds/data:sync/index.js
@@ -1,15 +1,38 @@
 'use strict';
 
 const co = require('co');
+const jsonfile = require('jsonfile');
+const checkMappingData = require('./check_mapping_data');
 
 module.exports = (program) => {
+    const logger = program.log;
 
-    const run = co.wrap(function *exec() {
+    function exit(err) {
+        if (!err) {
+            logger.info('Mappings file created successfully');
+            process.exit(0);
+        }
 
+        let errMessage = err.message;
+        if (err.code) {
+            errMessage = `${err.code}: ${errMessage}`;
+        }
+
+        logger.error(errMessage);
+        process.exit(1);
+    }
+
+    const run = co.wrap(function *exec(filename) {
+        try {
+            const data = jsonfile.readFileSync(filename);
+            checkMappingData(data);
+        } catch (err) {
+            exit(err);
+        }
     });
 
     program
-        .command('data:sync')
+        .command('data:sync <file>')
         .version('0.0.0')
         .description('Synchronizes data from postgres -> contentful')
         .action(run);
