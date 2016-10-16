4f8ee4fd7a9fdea6c23b5fd8e39b04a86f44ed2d
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-17 03:59:59 +0530

    Adds constraints

--- cmds/map:create/get_database_information.js
+++ cmds/map:create/get_database_information.js
@@ -3,6 +3,7 @@
 const co = require('co');
 const inquirer = require('inquirer');
 const PostgresSchema = require('pg-json-schema-export');
+const {has, mapValues, set} = require('lodash');
 
 function getDatabaseSchema(connectionString, schema = 'public') {
     return PostgresSchema.toJSON(connectionString, schema);
@@ -30,7 +31,13 @@ module.exports = co.wrap(function *exec(connectionURI, schemaName) {
             choices: co.wrap(function *getChoices({postgres}) {
                 const connection = postgres || process.env.PG_CONNECTION_URI;
                 const schema = yield getDatabaseSchema(connection, schemaName);
-                const tables = schema.tables;
+
+                const tables = mapValues(schema.tables, (value, tableName) => {
+                    if (has(schema.constraints, tableName)) {
+                        set(value, 'constraints', schema.constraints[tableName]);
+                    }
+                    return value;
+                });
 
                 return Object.keys(tables).map(name => ({
                     name,
