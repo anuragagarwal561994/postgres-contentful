8baec7033f24f9e317ee23c140c2f236139651f7
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-17 09:40:07 +0530

    Documents commander_helper

--- commander_helpers.js
+++ commander_helpers.js
@@ -2,13 +2,28 @@ const chalk = require('chalk');
 const isFunction = require('lodash/isFunction');
 
 module.exports = {
+  /**
+   * Colors the value and adds a default keyword to be displayed in help of a command
+   *
+   * @param value - represents a default value
+   * @returns {string}
+   */
   defaultString(value) {
     return chalk.cyan(`[default: ${value}]`);
   },
+
+  /**
+   * Exit module for the commands to end gracefully
+   *
+   * @param program
+   * @param [onSuccess] - to be called when program exits successfully
+   * @returns {function(*=)}
+   */
   exitModule(program, onSuccess) {
     const logger = program.log;
 
     return (err) => {
+      // return exit code 0 if no error
       if (!err) {
         if (isFunction(onSuccess)) {
           onSuccess();
@@ -16,12 +31,15 @@ module.exports = {
         process.exit(0);
       }
 
+      // if error display the error message with code (if present)
       let errMessage = err.message;
       if (err.code) {
         errMessage = `${err.code}: ${errMessage}`;
       }
 
       logger.error(errMessage);
+
+      // and exit with exit code 1
       process.exit(1);
     };
   },
