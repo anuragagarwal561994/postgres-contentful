d74155966684b83a714b58b971c831d14c38d1eb
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-17 21:09:18 +0530

    Documents fetch_data

--- cmds/data:sync/fetch_data.js
+++ cmds/data:sync/fetch_data.js
@@ -1,6 +1,17 @@
 const co = require('co');
 const pgp = require('pg-promise')();
 
+/**
+ * Connects to postgres database and fetches passed columns from given
+ * tableName using where condition
+ *
+ * @param {string} connection - postgres connecting string
+ * @param {string} tableName - postgres table name from where to fetch data
+ * @param {string[]} columns - columns to fetch
+ * @param {string} where='1=1' - where condition to query
+ * @throws when any error occurs with connection or fetching data from the database
+ * @return {Object[]}
+ */
 module.exports = co.wrap(function* exec(connection, tableName, columns, where = '1=1') {
   const db = pgp(connection);
 
