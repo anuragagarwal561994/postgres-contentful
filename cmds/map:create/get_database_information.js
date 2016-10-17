c9fd10c14f7364cf475a0501d641f2e3e122c85a
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-17 07:29:41 +0530

    Moves get_database_information questions to questions directory

--- cmds/map:create/get_database_information.js
+++ cmds/map:create/get_database_information.js
@@ -1,49 +1,12 @@
 const co = require('co');
 const inquirer = require('inquirer');
-const PostgresSchema = require('pg-json-schema-export');
-const { has, mapValues, set } = require('lodash');
 
-function getDatabaseSchema(connectionString, schema = 'public') {
-  return PostgresSchema.toJSON(connectionString, schema);
-}
+const askPostgres = require('../../questions/ask_postgres');
+const askRawTableSchema = require('../../questions/ask_rawTableSchema');
 
 module.exports = co.wrap(function* exec(connectionURI, schemaName) {
-  const question = [
-    {
-      name: 'postgres',
-      type: 'input',
-      message: 'Enter your postgreSQL connection string:',
-      default: connectionURI,
-      validate(connection) {
-        if (connection.length) {
-          return true;
-        }
-        return 'Please enter your postgreSQL connection string';
-      },
-      when: !process.env.PG_CONNECTION_URI,
-    },
-    {
-      name: 'rawTableSchema',
-      type: 'list',
-      message: 'Choose a table name:',
-      choices: co.wrap(function* getChoices({ postgres }) {
-        const connection = postgres || process.env.PG_CONNECTION_URI;
-        const schema = yield getDatabaseSchema(connection, schemaName);
+  const { postgres } = yield inquirer.prompt(askPostgres(connectionURI));
+  const { rawTableSchema } = yield inquirer.prompt(askRawTableSchema(schemaName, postgres));
 
-        const tables = mapValues(schema.tables, (value, tableName) => {
-          if (has(schema.constraints, tableName)) {
-            set(value, 'constraints', schema.constraints[tableName]);
-          }
-          return value;
-        });
-
-        return Object.keys(tables).map(name => ({
-          name,
-          value: tables[name],
-        }));
-      }),
-    },
-  ];
-
-  return yield inquirer.prompt(question);
+  return { postgres, rawTableSchema };
 });
---
+++ questions/ask_rawTableSchema.js
@@ -0,0 +1,27 @@
+const co = require('co');
+const PostgresSchema = require('pg-json-schema-export');
+const { has, mapValues, set } = require('lodash');
+
+const getDatabaseSchema = (connectionString, schema = 'public') => PostgresSchema.toJSON(connectionString, schema);
+
+module.exports = (schemaName, postgres) => ({
+  name: 'rawTableSchema',
+  type: 'list',
+  message: 'Choose a table name:',
+  choices: co.wrap(function* getChoices() {
+    const connection = postgres || process.env.PG_CONNECTION_URI;
+    const schema = yield getDatabaseSchema(connection, schemaName);
+
+    const tables = mapValues(schema.tables, (value, tableName) => {
+      if (has(schema.constraints, tableName)) {
+        set(value, 'constraints', schema.constraints[tableName]);
+      }
+      return value;
+    });
+
+    return Object.keys(tables).map(name => ({
+      name,
+      value: tables[name],
+    }));
+  }),
+});
