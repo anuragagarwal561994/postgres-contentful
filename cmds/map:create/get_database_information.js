1f7803648f9067c811832ed88abd8c1f67f7d018
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-10-17 09:11:33 +0530

    Doesn't ask postgres if already in enviromment vairbale

--- cmds/map:create/get_database_information.js
+++ cmds/map:create/get_database_information.js
@@ -1,10 +1,16 @@
 const co = require('co');
+const isUndefined = require('lodash/isUndefined');
 
 const getPostgres = require('../../questions/postgres');
 const getRawTableSchema = require('../../questions/rawTableSchema');
 
 module.exports = co.wrap(function* exec(connectionURI, schemaName) {
-  const { postgres } = yield getPostgres(connectionURI);
+  let postgres = process.env.PG_CONNECTION_URI;
+
+  if (isUndefined(postgres)) {
+    postgres = (yield getPostgres(connectionURI)).postgres;
+  }
+
   const { rawTableSchema } = yield getRawTableSchema(schemaName, postgres);
 
   return { postgres, rawTableSchema };
